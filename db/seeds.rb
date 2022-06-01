0194485614c254a71580aedfa637542639fda3b5
Alexy Castelneau <alexy.castelneau@gmail.com>
2022-06-01 17:23:54 +0200

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+azerty = { name: "azerty", address: "frtyhfctyhfcht", category: "chinese" }
+qsdfgh = { name: "qsdfgh", address: "fcthfcthgftcrgfctrgfctrg", category: "japanese" }
+wxcvbn = { name: "wxcvbn", address: "ftcrghftrcgftrcgftcrgftrcg", category: "belgian" }
+aqwzsx = { name: "aqwzsx", address: "yhjutuhyggygytgytgygy", category: "french" }
+
+[dishoom, pizza_east, azerty, qsdfgh, wxcvbn, aqwzsx].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
